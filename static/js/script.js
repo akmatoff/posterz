80e00f9cfc4e1a74e28c45375e330ead95c56b13
Azim Akmatov <azim.akmatov1@gmail.com>
2020-03-05 11:26:07 +0000

    Dropdown menu if mobile

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 const deleteForm = document.getElementById('delete-form');
+const dropdownContent = document.getElementById('dropdown-content');
+const dropdownButton = document.getElementById('dropdown-button');
+
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  dropdownButton.addEventListener('touchstart', showDropdownContent)
+}
+dropdownButton.addEventListener('click', showDropdownContent)
 
 // Confirm article deletion
 function deleteArticleWarning() {
@@ -8,3 +16,20 @@ function deleteArticleWarning() {
         deleteForm.removeAttribute('action');
     }
 }
+
+// Show dropdown
+function showDropdownContent() {
+  if (dropdownContent.style.display === 'none') {
+    dropdownContent.style.display = 'flex';
+  } else {
+    dropdownContent.style.display = 'none';
+  }
+}
+
+function closeDropdown() {
+  dropdownContent.style.display = 'none';
+  console.log('pressed');
+}
+
+// document.addEventListener('mousedown', closeDropdown)
+
